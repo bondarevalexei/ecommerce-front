b0fe9ada121c34c0cfbae279a4857905bca72f0d
bondarevalexei <gtrompy@gmail.com>
2023-12-21 19:44:56 +0300

    Initial Commit

---
+++ pages/api/webhook.js
@@ -0,0 +1,45 @@
+import {mongooseConnect} from "@/lib/mongoose";
+const stripe = require('stripe')(process.env.STRIPE_SK);
+import {buffer} from 'micro';
+import {Order} from "@/models/Order";
+
+const endpointSecret = "whsec_74d3d289e85c33908a562a46933debaa0d82dce74a8c4efb6fb2353e2fb65732";
+
+export default async function handler(req,res) {
+  await mongooseConnect();
+  const sig = req.headers['stripe-signature'];
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(await buffer(req), sig, endpointSecret);
+  } catch (err) {
+    res.status(400).send(`Webhook Error: ${err.message}`);
+    return;
+  }
+
+  // Handle the event
+  switch (event.type) {
+    case 'checkout.session.completed':
+      const data = event.data.object;
+      const orderId = data.metadata.orderId;
+      const paid = data.payment_status === 'paid';
+      if (orderId && paid) {
+        await Order.findByIdAndUpdate(orderId,{
+          paid:true,
+        })
+      }
+      break;
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  res.status(200).send('ok');
+}
+
+export const config = {
+  api: {bodyParser:false,}
+};
+
+// bright-thrift-cajole-lean
+// acct_1Lj5ADIUXXMmgk2a
